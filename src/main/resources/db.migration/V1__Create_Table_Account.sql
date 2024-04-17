e4f220411b5ce117d87135470bc43924b39511db
Vinicius Vieira <viniciusvsoares00@gmail.com>
2024-04-17 12:57:09 -0300

    implementing tb_account

---
+++ src/main/resources/db.migration/V1__Create_Table_Account.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_account(
+                           id bigint NOT NULL PRIMARY KEY,
+                           sort_code CHAR(8) NOT NULL,
+                           account_number CHAR(8) NOT NULL,
+                           current_balance NUMERIC(10,3) NOT NULL,
+                           bank_name VARCHAR(50) NOT NULL,
+                           owner_name VARCHAR(50) NOT NULL,
+                           UNIQUE (sort_code, account_number)
+)
